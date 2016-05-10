087012b080ac04225dc0560dfc834e0d50d1ffc3
gustavoHD <gustavo.dohara@gmail.com>
2016-05-09 23:59:13 -0300

    first commit

---
+++ src/main/java/ar/com/gustavoHD/spliterator/CreatingSpliterator.java
@@ -0,0 +1,37 @@
+package main.java.ar.com.gustavoHD.spliterator;
+
+import main.java.ar.com.gustavoHD.spliterator.model.Person;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * Created by gustavo on 09/05/16.
+ */
+public class CreatingSpliterator {
+
+    public static void main(String[] args){
+        Path path = Paths.get("/tmp/people.txt");
+
+        try (Stream<String> lines = Files.lines(path);) {
+            Spliterator<String> lineSpliterator = lines.spliterator();
+            Spliterator<Person> peopleSpliterator = new PersonSpliterator(lineSpliterator);
+
+            Stream<Person> peoples = StreamSupport.stream(peopleSpliterator, false);
+            peoples.forEach(System.out::println);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+}
